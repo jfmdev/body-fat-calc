dfad9536784e23d31c7ecbd738f0da183e734fbc
Jose <jfmdev@gmail.com>
2019-10-01 08:58:08 -0300

    Add required dependencies

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
+// Font Awesome.
+require("../node_modules/@fortawesome/fontawesome-free/css/all.min.css");
+
+// Bootstrap.
+import "bootstrap";
+require("../node_modules/bootstrap/dist/css/bootstrap.min.css");
+
+// Local storage.
+import VueLocalStorage from "vue-localstorage";
+Vue.use(VueLocalStorage);
+
+// Define application.
 new Vue({
   render: h => h(App)
 }).$mount("#app");
